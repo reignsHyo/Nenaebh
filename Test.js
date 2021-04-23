2b2106d6180dfb6177812748f936e3232d9eaf96
reignsHyo <82643853+reignsHyo@users.noreply.github.com>
2021-04-23 12:30:06 +0300

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,12 @@
+let daf = document.getElementById('da');
+daf.addEventListener("click", function(){
+      document.getElementById('Ask1').classList.add('active')
+    document.getElementById('otvet').classList.add('active');
+   document.getElementById('ask1_alert').classList.add('active');
+});
+let net = document.getElementById('net');
+net.addEventListener("click", function(){
+      document.getElementById('Ask1').classList.add('active')
+    document.getElementById('otvet').classList.add('active');
+   document.getElementById('ask1_alert').classList.add('active');
+});
